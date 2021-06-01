8296a5b1bf97d711a9db29709ca32f77f3d6b829
MalvinL645 <alvinfadil11@gmail.com>
2021-06-01 18:35:26 +0700

    change server

--- src/server.js
+++ src/server.js
@@ -26,7 +26,10 @@ const token = {
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: process.env.HOST || "localhost",
+    host:
+      process.env.NODE_ENV !== "production"
+        ? "localhost"
+        : "https://muhammadalvin.herokuapp.com",
   });
 
   try {
